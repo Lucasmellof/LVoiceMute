e91a1e91eb463d451293d23e57ee3d88c0d46231
Lucas <lucasmellocem12@gmail.com>
2020-12-16 23:49:17 -0300

    Adding exceptions

---
+++ src/main/kotlin/wtf/lucasmellof/voicemute/exception/NoPermissionException.kt
@@ -0,0 +1,7 @@
+package wtf.lucasmellof.voicemute.exception
+
+/* 
+ * @author Lucasmellof, Lucas de Mello Freitas created on 16/12/2020
+ */
+
+class NoPermissionException : PluginException("§cNo permission!")
---
+++ src/main/kotlin/wtf/lucasmellof/voicemute/exception/PluginException.kt
@@ -0,0 +1,6 @@
+package wtf.lucasmellof.voicemute.exception
+
+/* 
+ * @author Lucasmellof, Lucas de Mello Freitas created on 16/12/2020
+ */
+open class PluginException(message: String?) : Exception(message)
