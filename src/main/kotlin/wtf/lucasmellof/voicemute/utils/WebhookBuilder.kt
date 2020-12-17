d0d7e9631a1f85b1f056e51e4eaea8ac392bb051
Lucas <lucasmellocem12@gmail.com>
2020-12-17 11:32:21 -0300

    Updating webhook builder

--- src/main/kotlin/wtf/lucasmellof/voicemute/utils/WebhookBuilder.kt
+++ src/main/kotlin/wtf/lucasmellof/voicemute/utils/WebhookBuilder.kt
@@ -4,7 +4,10 @@ import club.minnced.discord.webhook.WebhookClient
 import club.minnced.discord.webhook.WebhookClientBuilder
 import club.minnced.discord.webhook.send.WebhookEmbedBuilder
 import wtf.lucasmellof.voicemute.LVoiceMute
-import java.util.concurrent.ThreadFactory
+import club.minnced.discord.webhook.send.WebhookMessageBuilder
+
+
+
 
 /* 
  * @author Lucasmellof, Lucas de Mello Freitas created on 16/12/2020
@@ -13,16 +16,24 @@ class WebhookBuilder(private val plugin: LVoiceMute) {
     var webhook: WebhookClient? = null
     fun sendWebhook(message: WebhookEmbedBuilder) {
         if (webhook == null) return
-        webhook!!.send(message.build())
+        val builder = WebhookMessageBuilder()
+        builder.setUsername("LVoiceMute") // use this username
+        builder.addEmbeds(message.build())
+        webhook!!.send(builder.build())
     }
     fun load() {
-        val builder = WebhookClientBuilder(plugin.configCache.webhook)
+        var url = plugin.configCache.webhook
+        val builder = WebhookClientBuilder(url)
         builder.setThreadFactory { job: Runnable? ->
             val thread = Thread(job)
-            thread.name = "WebookThread"
+            thread.name = "WebhookThread"
             thread.isDaemon = true
             thread
         }
         webhook = builder.build()
+    }
+    fun reload() {
+        webhook = null
+        load()
     }
 }
