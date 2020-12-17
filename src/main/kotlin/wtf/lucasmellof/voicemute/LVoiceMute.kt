5e38c4f117bbf4fd4ead4df7c1e31d913d229833
Lucas <lucasmellocem12@gmail.com>
2020-12-16 23:46:41 -0300

    Adding LVoiceMute

---
+++ src/main/kotlin/wtf/lucasmellof/voicemute/LVoiceMute.kt
@@ -0,0 +1,44 @@
+package wtf.lucasmellof.voicemute
+
+import org.bukkit.plugin.java.JavaPlugin
+import wtf.lucasmellof.voicemute.commands.CheckMuteCommand
+import wtf.lucasmellof.voicemute.commands.UnVoiceMuteCommand
+import wtf.lucasmellof.voicemute.commands.VoiceMuteCommand
+import wtf.lucasmellof.voicemute.listeners.PlayerListener
+import wtf.lucasmellof.voicemute.user.UsersManager
+import wtf.lucasmellof.voicemute.utils.ConfigCache
+import wtf.lucasmellof.voicemute.utils.WebhookBuilder
+import wtf.lucasmellof.voicemute.utils.YamlConfig
+
+/* 
+ * @author Lucasmellof, Lucas de Mello Freitas created on 16/12/2020
+ */
+class LVoiceMute : JavaPlugin() {
+    private var users: UsersManager? = null
+    lateinit var webhook: WebhookBuilder
+    lateinit var configCache: ConfigCache
+
+    override fun onLoad() {
+        users = UsersManager(this)
+    }
+
+    override fun onEnable() {
+        loadModules()
+    }
+
+    private fun loadModules() {
+        configCache = ConfigCache(this)
+        configCache.load()
+        configCache.loadCache()
+        UnVoiceMuteCommand(this)
+        VoiceMuteCommand(this)
+        CheckMuteCommand(this)
+        webhook = WebhookBuilder(this)
+        webhook.load()
+        PlayerListener(this)
+    }
+
+    fun getUsers(): UsersManager? {
+        return users
+    }
+}
